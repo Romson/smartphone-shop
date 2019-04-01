09895b66073bbd7133e298e8d1b171d8ebf3a143
romson <romerolijkwan@gmail.com>
2019-04-01 15:44:33 +0200

    edit buttons

--- src/components/Product.js
+++ src/components/Product.js
@@ -65,9 +65,10 @@ const ProductWrapper = styled.div`
 }
 .img-container {
   position: relative;
+  overflow: hidden;
 }
 .card-img-top {
-  transition: all 0.5s linear;
+  transition: all 1s linear;
 }
 .img-container:hover .card-img-top {
   transform: scale(1.2);
@@ -77,5 +78,18 @@ const ProductWrapper = styled.div`
   bottom: 0;
   right: 0;
   padding: 0.2rem 0.4rem;
+  background: var(--moderateCyan);
+  border: none;
+  color: var(--mainWhite);
+  font-size: 1.4rem;
+  border-radius: 0.5rem 0 0 0;
+  transform: translate(100%,100%);
+  transition: all 0.3s linear;
+}
+.img-container:hover .cart-btn {
+  transform: translate(0, 0);
+}
+.cart-btn:hover {
+  cursor: pointer;
 }
 `;
